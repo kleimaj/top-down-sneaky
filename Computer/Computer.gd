d67a6a3230db3a80881b6605b116a2f24cf3e8de
kleimaj <jacobakleiman@gmail.com>
2021-01-27 14:59:06 -0800

    connects locked doors with computers

--- Computer/Computer.gd
+++ Computer/Computer.gd
@@ -2,6 +2,26 @@ extends Node2D
 
 var can_click = false
 
+var combination
+
+export var combination_length = 4
+export var lock_group = "Unset"
+
+signal combination
+
+func _ready():
+	generate_combination()
+	emit_signal("combination", combination, lock_group)
+	$Label.text = lock_group
+	$Label.rect_rotation = -rotation_degrees
+
+func generate_combination():
+	combination = CombinationGenerator.generate_combination(combination_length)
+	set_popup_text()
+
+func set_popup_text():
+	$CanvasLayer/ComputerPopup.set_text(combination)
+
 func _on_Area2D_body_entered(body):
 	can_click = true
 
@@ -9,8 +29,10 @@ func _on_Area2D_body_entered(body):
 func _on_Area2D_body_exited(body):
 	can_click = false
 	$CanvasLayer/ComputerPopup.hide()
+	$Light2D.enabled = false	
 
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
 		$CanvasLayer/ComputerPopup.popup_centered()
+		$Light2D.enabled = true
